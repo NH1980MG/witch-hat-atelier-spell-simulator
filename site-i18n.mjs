6230968730f4490539175a92d7926189d58a7b3d
NH1980MG <nathanhung.hwl@gmail.com>
2026-08-30 22:37:27 -0400

    feat: add stroke annotations and smoothing

--- site-i18n.mjs
+++ site-i18n.mjs
@@ -2,7 +2,7 @@ import {
   DEFAULT_LOCALE,
   resolveLocale,
   translate,
-} from "./i18n.mjs?v=20260829-parametric-rings-v1";
+} from "./i18n.mjs?v=20260830-stroke-annotations-v1";
 
 let currentLocale = DEFAULT_LOCALE;
 
